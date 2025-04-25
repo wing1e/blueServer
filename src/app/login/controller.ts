a3f27e87c86c664aeaa0963ec50679492446ef0c
wing1e <826232419@qq.com>
2025-04-25 23:04:19 +0800

    删除dist的追踪

--- src/app/login/controller.ts
+++ src/app/login/controller.ts
@@ -1,8 +1,12 @@
-import { log } from "console";
+
 import { Request, Response, NextFunction } from "express";
+import { getSession } from "./service";
 
 // 微信登录
 export const wxLogin = async (req: Request, res: Response, next: NextFunction) => {
-    log(req.body);
+    const code = req.body?.code;
+    const userinfo =await getSession(code);
+    console.log(userinfo);
+    
     res.send({mes:'请求成功'});
 };
--- src/app/login/service.ts
+++ src/app/login/service.ts
@@ -1,2 +1,13 @@
+import axios from "axios";
+import { APP_ID, APP_SECRET } from "../app.config";
 
-
+export const getSession =async (code:string) => {
+    try{
+        const code2Session = `https://api.weixin.qq.com/sns/jscode2session?appid=${APP_ID}&secret=${APP_SECRET}&js_code=${code}&grant_type=authorization_code`
+        const result = await axios.get(code2Session);
+        return result.data;
+        
+    }catch(err){
+        
+    }
+};
